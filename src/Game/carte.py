198635ddf2db6d48abec583eef194cf312dff7c9
Alexis <huret.alexis28@gmail.com>
2026-01-20 16:34:57 +0100

    implémentation class deck et cartes

--- src/Game/carte.py
+++ src/Game/carte.py
@@ -1,4 +1,27 @@
 class Carte:
 
-    def __init__(self):
-        pass
+    dict_transi_val = {1:"A",2:"2",3:"3",4:"4",5:"5",6:"6",7:"7",8:"8",9:"9",10:"10",11:"V",12:"D",13:"R"}
+
+    dict_transi_cou = {1:"coeur",2:"carreau",3:"pique",4:"trèfle"}
+
+    def __init__(self,val : int, couleur : int) -> "Carte":
+        self.__val : int = val
+        self.__cou : int = couleur
+
+    def __get_val(self) -> int:
+        return self.__val
+    
+    def __set_val(self,val : int) -> None :
+        self.__val = val
+
+    def __get_cou(self) -> int:
+        return self.__cou
+    
+    def __set_cou(self,couleur : int) -> None :
+        self.__cou = couleur
+
+    val = property(__get_val,__set_val)
+    cou = property(__get_cou,__set_cou)
+
+    def __str__(self) -> str:
+        return f"{self.dict_transi_val[self.val]} de {self.dict_transi_cou[self.cou]}"
---
+++ src/Game/deck.py
@@ -0,0 +1,70 @@
+from carte import Carte
+import random
+
+class Deck:
+
+    def __init__(self) -> "Deck":
+        self.__contenue : list[Carte] = []
+
+    def __get_contenue(self) -> list[Carte]:
+        return self.__contenue
+    
+    def __set_contenue(self,contenue : list[Carte]) -> None :
+        self.__contenue = contenue
+
+    contenue = property(__get_contenue,__set_contenue)
+    
+    def est_vide(self) -> bool:
+        return len(self.contenue) == 0
+    
+    def tirer_carte(self) -> Carte|None:
+        if self.est_vide():
+            return None
+        return self.contenue.pop(0)
+    
+    def rajouter_carte(self,carte : Carte) -> None:
+        self.contenue.append(carte)
+
+    def melanger(self) -> None:
+        cartes = self.contenue
+        self.contenue = []
+        while len(cartes) != 0:
+            self.contenue.append(cartes.pop(random.randint(0,len(cartes)-1)))
+
+    def reset_52(self) -> None:
+        self.contenue = []
+        for cou in range(1,5):
+            for val in range(1,14):
+                self.rajouter_carte(Carte(val,cou))
+        self.melanger()
+
+    def vider_dans(self,deck2 : "Deck"):
+        while not self.est_vide():
+            deck2.rajouter_carte(self.tirer_carte())
+
+    
+    
+
+
+
+if __name__ == "__main__":
+    mon_deck = Deck()
+    # print(mon_deck.est_vide())
+    # mon_deck.rajouter_carte(Carte(10,3))
+    # mon_deck.rajouter_carte(Carte(13,2))
+    # print(mon_deck.est_vide())
+    # print(mon_deck.tirer_carte())
+    # print(mon_deck.tirer_carte())
+    # print(mon_deck.est_vide())
+    # mon_deck.rajouter_carte(Carte(1,3))
+    # mon_deck.rajouter_carte(Carte(3,2))
+    # mon_deck.rajouter_carte(Carte(10,4))
+    # mon_deck.rajouter_carte(Carte(13,1))
+    # mon_deck.melanger()
+    # print(mon_deck.tirer_carte())
+    # print(mon_deck.tirer_carte())
+    # print(mon_deck.tirer_carte())
+    # print(mon_deck.tirer_carte())
+    mon_deck.reset_52()
+    for _ in range(10):
+        print(mon_deck.tirer_carte())

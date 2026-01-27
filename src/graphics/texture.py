6eccea39fcbe15f6da43a3ebfbf61aafd8b5c42c
Nathan Gabard <nathangabard@outlook.fr>
2026-01-27 17:22:21 +0100

    27-01

--- src/graphics/texture.py
+++ src/graphics/texture.py
@@ -1,59 +1,53 @@
 from OpenGL.GL import *
 from PIL import Image
 from math import ceil,sqrt
+from dataclasses import dataclass
+
 class Texture:
     def __init__(self,path:str):
         self.__path = path
-        self.__opengl_id = self.__generer_texture(path) # id de la texture
-    
-    def __generer_texture(self,path:str):
-        image = Image.open(path).convert("RGBA")
-        width, height = image.size
-        image_data = image.tobytes()
-
-        texture_id = glGenTextures(1)
-        glBindTexture(GL_TEXTURE_2D, texture_id)
-
-        # parametre
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR)
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
-
-        glTexImage2D(GL_TEXTURE_2D,0,GL_RGBA,width,height,0,GL_RGBA,GL_UNSIGNED_BYTE,image_data)
-        glGenerateMipmap(GL_TEXTURE_2D)
-        glBindTexture(GL_TEXTURE_2D, 0)
-        return texture_id
-    
-    def __getId(self): return self.__opengl_id
+        
     def __getPath(self): return self.__path
     
-    id = property(__getId)
     chemin = property(__getPath)
+    
+@dataclass(frozen=True) # dit que c'est une class qui contient que des attributs qui ne peuvent pas etre modifier
+class UV:
+    """
+    Coordonnées dans la texture
+    """
+    u0:float
+    v0:float
+    u1:float
+    v1:float
     
 class TextureManager:
     atlas_id = -1
     taille_texture = 128
+    __textures_data:list[Image.Image] = []
+    __uvs:list[UV] = []
     
-    def __init__(self):
-        self.__textures_data:list[Image] = [] #type: ignore
-    
-    def __generer_atlas_texture(self,taille,data):
+    @classmethod
+    def __generer_atlas_texture(cls,image:Image.Image) -> int:
+        data = image.tobytes()
+        largeur, hauteur = image.size
         texture_id = glGenTextures(1)
         glBindTexture(GL_TEXTURE_2D, texture_id)
 
         # parametre
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR)
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
 
-        glTexImage2D(GL_TEXTURE_2D,0,GL_RGBA,taille,taille,0,GL_RGBA,GL_UNSIGNED_BYTE,data)
+        glTexImage2D(GL_TEXTURE_2D,0,GL_RGBA,largeur,hauteur,0,GL_RGBA,GL_UNSIGNED_BYTE,data)
         glGenerateMipmap(GL_TEXTURE_2D)
         glBindTexture(GL_TEXTURE_2D, 0)
+        
         return texture_id
     
-    def creer(self,path:str):
+    @classmethod
+    def creer(cls,path:str) -> int:
         """
         Ajoute les donnees de l'image à la liste des textures
         Args:
@@ -62,30 +56,46 @@ class TextureManager:
         print(f"Chargement de {path}")
         image = Image.open(path).convert("RGBA")
         width, height = image.size
-        assert width==TextureManager.taille_texture and height==TextureManager.taille_texture, f"Les images doivents être {TextureManager.taille_texture}x{TextureManager.taille_texture}"
-        self.__textures_data.append(image.load())
+        assert width==cls.taille_texture and height==cls.taille_texture, f"Les images doivents être {cls.taille_texture}x{cls.taille_texture},({width},{height})"
+        cls.__textures_data.append(image.copy())
+        return len(cls.__textures_data)-1 # ID
         
-    def generer_texture_atlas(self): # TODO: créé un rectangle (Rectangle Packing Algorithm)
+    @classmethod
+    def generer_texture_atlas(cls): # TODO: créé un rectangle (Rectangle Packing Algorithm)
         """
         Créé une grande texture carré qui contient toutes les autres
         """
-        n = len(self.__textures_data)
+        n = len(cls.__textures_data)
         taille_coter = ceil(sqrt(n))
-        taille_atlas = taille_coter*TextureManager.taille_texture
-        print(f"Creation de l'atlas pour {n} images, carré de {taille_coter} textures de coté et {taille_atlas} pixels")
-        atlas = Image.new("RGBA",(taille_atlas,taille_atlas),(0,0,0,0))
-        atlas_data = atlas.load()
-        for y in range(taille_atlas):
-            for x in range(taille_atlas):
-                atlas_data[x,y] = self.__textures_data[x//TextureManager.taille_texture][x%TextureManager.taille_texture,y%TextureManager.taille_texture] # TODO: finir la creation de l'atlas texture (spritesheet)
+        cls.taille_atlas = taille_coter*TextureManager.taille_texture
+        print(f"Creation de l'atlas pour {n} images, carré de {taille_coter} textures de coté et {cls.taille_atlas} pixels")
+        atlas = Image.new("RGBA",(cls.taille_atlas,cls.taille_atlas),(0,0,0,0))
+        for i,img in enumerate(cls.__textures_data):
+            x = (i%taille_coter)*cls.taille_texture
+            y = (i//taille_coter)*cls.taille_texture
+            atlas.paste(img,(x,y))
+            u0 = x/cls.taille_atlas
+            v0 = y/cls.taille_atlas
+            u1 = (x+cls.taille_texture)/cls.taille_atlas
+            v1 = (y+cls.taille_texture)/cls.taille_atlas
+            cls.__uvs.append(UV(u0,v0,u1,v1))
+            
         
         atlas.save("test.png",format="PNG")
+        
+        cls.atlas_id = cls.__generer_atlas_texture(atlas)
+    
+    
+    
+    @classmethod
+    def getUV(cls,index):
+        return cls.__uvs[index]
 
 
 if __name__ == "__main__":
-    t = TextureManager()
-    t.creer("test2.png")
-    t.creer("test1.png")
-    t.creer("test2.png")
-    t.creer("test1.png")
-    t.generer_texture_atlas()
+    TextureManager.creer("test2.png")
+    TextureManager.creer("test1.png")
+    TextureManager.creer("test1.png")
+    TextureManager.creer("test2.png")
+    
+    id = TextureManager.generer_texture_atlas()

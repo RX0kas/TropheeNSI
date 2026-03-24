968b7305da3c48251804b1ea5254524ebdbfea96
Nathan Gabard <nathangabard@outlook.fr>
2026-03-24 16:35:04 +0100

    Vec4

--- src/math/vectors.py
+++ src/math/vectors.py
@@ -61,3 +61,35 @@ class Vec3:
     
     NOIR = property(__noir)
     BLANC = property(__blanc)
+    
+class Vec4:
+    def __init__(self, x:float=0, y:float=0,z:float=0,w:float=0):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.w = w
+
+    def __getitem__(self, key) -> float: # pour faire Vec3[1]
+        assert key in [0,1,2,3]
+        if key==0:
+            return self.x
+        elif key==1:
+            return self.y
+        elif key==2:
+            return self.z
+        return self.w
+        
+    def __setitem__(self, key, value:float) -> None: # pour faire Vec4[1] = 5
+        assert key in [0,1,2,3]
+        if key==0:
+            self.x = value
+        elif key==1:
+            self.y = value
+        elif key==2:
+            self.z = value
+        else:
+            self.w = value
+    
+    @classmethod
+    def get_gpu_memory_size(cls):
+        return 3*sizeof(c_float)
